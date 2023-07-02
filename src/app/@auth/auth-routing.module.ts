2042da4c16d033f4a6388e9883485c1fbe9a9c56
temidevgirl <esthertemitope17@gmail.com>
2023-07-02 01:31:31 +0100

    forgot-password page

--- src/app/@auth/auth-routing.module.ts
+++ src/app/@auth/auth-routing.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { LoginComponent } from "./login/login.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 const routes: Routes = [
-    {path:'', component:LoginComponent}
-]
+  { path: '', component: LoginComponent },
 
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports:[RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class AuthRoutingModule{}
+export class AuthRoutingModule {}
