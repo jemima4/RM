71d8c773e37cb4998d0927af9cadcfc7bcde1a61
Kolawole <omotoshok9@gmail.com>
2023-07-03 16:46:44 +0100

    added inventory module

--- src/app/@components/webapp/webapp.module.ts
+++ src/app/@components/webapp/webapp.module.ts
@@ -7,9 +7,11 @@ import { WebappComponent } from './webapp.component';
 import { MaterialModule } from 'src/app/@core/styles/material/material.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProcurementComponent } from './procurement/procurement.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { ProductsCategoryComponent } from './inventory/products-category/products-category.component';
 
 @NgModule({
-  declarations: [WebappComponent, SidebarComponent, DashboardComponent, ProcurementComponent],
+  declarations: [WebappComponent, SidebarComponent, DashboardComponent, ProcurementComponent, InventoryComponent, ProductsCategoryComponent],
   imports: [CommonModule, WebappRoutingModule, MaterialModule],
 })
 export class WebappModule {}
