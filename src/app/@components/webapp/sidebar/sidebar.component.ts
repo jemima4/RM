48e18f72e49c2d4229667a1c2f3c2bb49aa7fa70
Kolawole <omotoshok9@gmail.com>
2023-07-03 15:05:39 +0100

    Sidebar initial completion

--- src/app/@components/webapp/sidebar/sidebar.component.ts
+++ src/app/@components/webapp/sidebar/sidebar.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  HostListener,
+  OnInit,
+  Output,
+} from '@angular/core';
+import {
+  animate,
+  keyframes,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('350ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('350ms', style({ opacity: 0 })),
+      ]),
+    ]),
+    trigger('rotate', [
+      transition(':enter', [
+        animate(
+          '1000ms',
+          keyframes([
+            style({ transform: 'rotate(0deg)', offset: '0' }),
+            style({ transform: 'rotate(2turn)', offset: '1' }),
+          ])
+        ),
+      ]),
+    ]),
+  ],
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  @Output() onToggleSideNav = new EventEmitter<SideNavToggle>();
 
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+  }
+
+  collapsed = false;
+  screenWidth = 0;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth = window.innerWidth;
+    if (this.screenWidth <= 768) {
+      this.collapsed = false;
+      this.onToggleSideNav.emit({
+        collapsed: this.collapsed,
+        screenWidth: this.screenWidth,
+      });
+    }
+  }
+
+  toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth,
+    });
+  }
+
+  closeSidenav(): void {
+    this.collapsed = false;
+    this.onToggleSideNav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth,
+    });
+  }
 }
--- src/app/@components/webapp/webapp.module.ts
+++ src/app/@components/webapp/webapp.module.ts
@@ -5,9 +5,11 @@ import { WebappRoutingModule } from './webapp-routing.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { WebappComponent } from './webapp.component';
 import { MaterialModule } from 'src/app/@core/styles/material/material.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProcurementComponent } from './procurement/procurement.component';
 
 @NgModule({
-  declarations: [WebappComponent, SidebarComponent],
+  declarations: [WebappComponent, SidebarComponent, DashboardComponent, ProcurementComponent],
   imports: [CommonModule, WebappRoutingModule, MaterialModule],
 })
 export class WebappModule {}
