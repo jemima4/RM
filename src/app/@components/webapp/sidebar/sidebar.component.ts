5356f560b219db34a98206b57260ccfc87d9012b
Kolawole <omotoshok9@gmail.com>
2023-07-03 15:42:51 +0100

    transition fix

--- src/app/@components/webapp/sidebar/sidebar.component.ts
+++ src/app/@components/webapp/sidebar/sidebar.component.ts
@@ -30,13 +30,13 @@ interface SideNavToggle {
       ]),
       transition(':leave', [
         style({ opacity: 1 }),
-        animate('350ms', style({ opacity: 0 })),
+        animate('200ms', style({ opacity: 0 })),
       ]),
     ]),
     trigger('rotate', [
       transition(':enter', [
         animate(
-          '1000ms',
+          '500ms',
           keyframes([
             style({ transform: 'rotate(0deg)', offset: '0' }),
             style({ transform: 'rotate(2turn)', offset: '1' }),
